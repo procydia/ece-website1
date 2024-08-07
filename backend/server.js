764de1a97fb393001707a9d8b19aa844405c47a6
Mehul Sharma <sharma.mehul8448@gmail.com>
2024-08-07 15:17:46 +0530

    ..

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ const connectDB = require('./config/db');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const path = require('path');
+const upload = require('multer');
 
 const app = express();
 
@@ -13,8 +14,7 @@ connectDB();
 app.use(express.json({ extended: false }));
 app.use(cors());
 
-// Serve static files from the 'uploads' directory
-app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+
 
 // Define Routes
 app.use('/api/users', require('./routes/users'));
@@ -24,6 +24,8 @@ app.use('/api/profile', require('./routes/profile'));
 app.use('/api/otp', require('./routes/otp'));
 app.use('/api/vendor', require('./routes/vendor'));
 app.use('/api/reviews', require('./routes/reviews'));
+// Serve static files from the 'uploads' directory
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 const PORT = process.env.PORT || 5000;
 
